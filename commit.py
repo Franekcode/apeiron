d84f0fe05a3c6aa2f77ea7249836bcb78b89a8e9
PatrickStojek <patryk.stojek7@gmail.com>
2024-09-28 14:39:24 +0200

    basic implementation of commit.py

--- commit.py
+++ commit.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import requests
+from bs4 import BeautifulSoup
+
+# Define the URL to scrape
+url = 'http://example.com'
+
+# Make a request to the URL
+response = requests.get(url)
+
+# Parse the HTML content using BeautifulSoup
+soup = BeautifulSoup(response.content, 'html.parser')
+
+# Print the parsed content
+print(soup.prettify())
